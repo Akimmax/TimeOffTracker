67335ad0e26cde6ec2b73ad34bf25edb89d8f886
Anatoliy Olexseenko <Olexseenko@outlook.com>
2018-10-15 12:36:50 +0300

    Added BasicRepositoriry + UnitOfWork

---
+++ TOT.Data/TOTUnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using TOT.Data.Repositories;
+using TOT.Entities.Request_Entities;
+using TOT.Interfaces;
+
+namespace TOT.Data
+{
+    public class TOTUnitOfWork : IUnitOfWork
+    {
+        private readonly DBContext dbContext;
+
+        public TOTUnitOfWork(DBContext context)
+        {
+            dbContext = context;
+            TimeOffRequests = new BasicRepository<TimeOffRequest>(context);
+            TimeOffTypes = new BasicRepository<TimeOffType>(context);
+            Checks = new BasicRepository<Check>(context);
+            RequstStatuses = new BasicRepository<RequestStatus>(context);
+        }
+
+        public IRepository<TimeOffRequest> TimeOffRequests { get; }
+        public IRepository<TimeOffType> TimeOffTypes { get; }
+        public IRepository<Check> Checks { get; }
+        public IRepository<RequestStatus> RequstStatuses { get; }
+
+        public Task SaveAsync()
+        {
+            return dbContext.SaveChangesAsync();
+        }
+    }
+}
--- TOT.Interfaces/IUnitOfWork.cs
+++ TOT.Interfaces/IUnitOfWork.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 using TOT.Entities.Request_Entities;
 
 namespace TOT.Interfaces
 {
     public interface IUnitOfWork
     {
-        IRepository<TimeOffRequest> TimeOfRequests { get; }
-        IRepository<TimeOffType> TimeOfTypes { get; }
+        IRepository<TimeOffRequest> TimeOffRequests { get; }
+        IRepository<TimeOffType> TimeOffTypes { get; }
         IRepository<Check> Checks { get; }
+        IRepository<RequestStatus> RequstStatuses { get; }
+
+        Task SaveAsync();
     }
 }
