7d879a0965538c96c2477fd4a5ae155013092adf
Anatoliy Olexseenko <Olexseenko@outlook.com>
2018-10-17 22:24:12 +0300

    Revert "Update Check.cs"
    This reverts commit 67354d28a38a483b9eeec087959a50f38b192067.

--- TOT.Entities/Request_Entities/Check.cs
+++ TOT.Entities/Request_Entities/Check.cs
@@ -6,7 +6,7 @@ namespace TOT.Entities.Request_Entities
     {
         public int Id { get; set; }
         public string UserId { get; set; }//employee who should accept request
-        public DateTime SolvedDate { get; set; }// date of decision
+        public DateTimeOffset SolvedDate { get; set; }// date of decision
         public string Reason { get; set; }// reason of decision
         public RequestStatus Status { get; set; }// status of request {requsted, in_progres, denied, accepted}
 
