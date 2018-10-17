19b202167c9ac16f0550f4aa359f0969ed8bc6d0
Anatoliy Olexseenko <Olexseenko@outlook.com>
2018-10-17 22:26:37 +0300

    Added basic mapping

--- TOT.Data/TOTDBContext.cs
+++ TOT.Data/TOTDBContext.cs
@@ -9,8 +9,49 @@ namespace TOT.Data
         {
         }
 
+        public DbSet<TimeOffRequest> TimeOffRequests { get; }
+        public DbSet<TimeOffType> TimeOffTypes { get; }
+        public DbSet<Check> Checks { get; }
+        public DbSet<RequestStatus> RequestStatuses { get; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<TimeOffRequest>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<TimeOffRequest>()
+                .HasOne(x => x.TimeOffType)
+                .WithMany()
+                .IsRequired();
+
+            modelBuilder.Entity<TimeOffType>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<TimeOffType>()
+                .Property(x => x.Title)
+                .IsRequired();
+
+            modelBuilder.Entity<Check>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Check>()
+                .HasOne(x => x.Status)
+                .WithMany()
+                .IsRequired();
+            modelBuilder.Entity<Check>()
+                .HasOne(x => x.TimeOffRequest)
+                .WithMany(x => x.Checks)
+                .IsRequired();
+
+            modelBuilder.Entity<RequestStatus>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<RequestStatus>()
+                .Property(x => x.Title)
+                .IsRequired();
+            modelBuilder.Entity<RequestStatus>()
+                .HasData(
+                new RequestStatus { Title = "Requsted", Id = (int)Statuses.Requsted },
+                new RequestStatus { Title = "In progres", Id = (int)Statuses.InProgres },
+                new RequestStatus { Title = "Denied", Id = (int)Statuses.Denied },
+                new RequestStatus { Title = "Accepted", Id = (int)Statuses.Accepted });
+
             base.OnModelCreating(modelBuilder);
         }
     }
