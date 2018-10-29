625fe3bd313380a0e3b2d4f6b07a4f57d05d1015
Максим Акименко <Akymenko.M.O@nmu.one>
2018-10-29 22:57:32 +0200

    Add DTOs

---
+++ TOT.Dto/TimeOffRequests/TimeOffRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TOT.Dto.TimeOffRequests
+{
+    public class TimeOffRequestDTO
+    {
+        public TimeOffRequestDTO()
+        {
+            Approvals = new List<TimeOffRequestApprovalDTO>();
+        }
+        public int Id { get; set; }
+        public string User { get; set; }
+        public TimeOffTypeDTO Type { get; set; }
+        public DateTime StartsAt { get; set; }
+        public DateTime? EndsOn { get; set; }
+        public string Note { get; set; }
+
+        public ICollection<TimeOffRequestApprovalDTO> Approvals { get; set; }
+
+    }
+}
