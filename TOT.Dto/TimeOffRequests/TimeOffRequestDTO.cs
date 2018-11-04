4e84065c514782160a6cbc29e292320a5541160f
Максим Акименко <Akymenko.M.O@nmu.one>
2018-11-05 00:29:43 +0200

    Some changes in Dto

--- TOT.Dto/TimeOffRequests/TimeOffRequestDTO.cs
+++ TOT.Dto/TimeOffRequests/TimeOffRequestDTO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using TOT.Dto.CustomValidationAttributes;
 
 namespace TOT.Dto.TimeOffRequests
 {
@@ -11,11 +12,21 @@ namespace TOT.Dto.TimeOffRequests
             Approvals = new List<TimeOffRequestApprovalDTO>();
         }
         public int Id { get; set; }
+
         public string User { get; set; }
-        public int TypeId { get; set; }
+
         public TimeOffTypeDTO Type { get; set; }
+
+        [Display(Name = "Type")]
+        public int TypeId { get; set; }
+
+        [Display(Name = "Start")]
+        [DateLessThan("EndsOn", ErrorMessage = "Start date must be earlier than End date")]
         public DateTime StartsAt { get; set; }
+
+        [Display(Name = "End")]
         public DateTime? EndsOn { get; set; }
+
         public string Note { get; set; }
 
         public ICollection<TimeOffRequestApprovalDTO> Approvals { get; set; }
