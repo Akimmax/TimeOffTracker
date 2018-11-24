a16712a57b24c50e291e6ed80c333f132fa01933
Anatoliy Olexseenko <Olexseenko@outlook.com>
2018-11-24 18:36:15 +0200

    Added roles initializer

--- TOT.Entities/IdentityEntities/User.cs
+++ TOT.Entities/IdentityEntities/User.cs
@@ -7,6 +7,10 @@ namespace TOT.Entities.IdentityEntities
 {
     public class User : IdentityUser
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+
         public int PositionId { get; set; }
         public EmployeePosition Position { get; set; }
         public DateTime HireDate { get; set; }
